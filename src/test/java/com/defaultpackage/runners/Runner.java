042e4f6e36390f0ca36a5827e04da4c55a5e0dfc
SQARafael <rafael.chica@sqasa.co>
2023-08-17 19:32:53 -0500

    add snippets on runners

--- src/test/java/com/defaultpackage/runners/Runner.java
+++ src/test/java/com/defaultpackage/runners/Runner.java
@@ -3,11 +3,15 @@ package com.defaultpackage.runners;
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.devtools.v85.cachestorage.model.Cache;
 
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = "src/test/resources/features/default.feature"
+        features = "src/test/resources/features/default.feature",
+        glue = "com.defaultpackage.stepDef",
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        publish = true
         )
 public class Runner {
 }
