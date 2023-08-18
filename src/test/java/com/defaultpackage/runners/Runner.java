bbca8b0b0135cf6a6234ba1ec906890b57056a11
SQARafael <rafael.chica@sqasa.co>
2023-08-17 19:31:14 -0500

    add Runner inicial

---
+++ src/test/java/com/defaultpackage/runners/Runner.java
@@ -0,0 +1,13 @@
+package com.defaultpackage.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/default.feature"
+        )
+public class Runner {
+}
