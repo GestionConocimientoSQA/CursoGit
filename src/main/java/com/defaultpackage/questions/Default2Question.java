939b0a026b53fec46617c63f21f55b69ce113493
SQARafael <rafael.chica@sqasa.co>
2023-08-22 12:56:12 -0500

    add Question2Default

---
+++ src/main/java/com/defaultpackage/questions/Default2Question.java
@@ -0,0 +1,22 @@
+package com.defaultpackage.questions;
+/*
+ * @(#) Default2Question.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class Default2Question implements Question<String> {
+    private static String name;
+
+
+    @Override
+    public String answeredBy(Actor actor) {
+        return null;
+    }
+}
