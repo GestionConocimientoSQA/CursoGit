13548cbe4dc5cb85d21123d214dc324a11ffa681
SQARafael <rafael.chica@sqasa.co>
2023-08-22 12:00:26 -0500

    implement I Task default2task

--- src/main/java/com/defaultpackage/tasks/Default2Task.java
+++ src/main/java/com/defaultpackage/tasks/Default2Task.java
@@ -4,9 +4,16 @@ package com.defaultpackage.tasks;
  * Copyright 2023 SQA. Todos los derechos reservados.
  */
 
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+
 /**
  * @autor Rafael Chica
  * @Fecha: --o--
  */
-public class Default2Task {
+public class Default2Task implements Task {
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+
+    }
 }
