ad8ad645faafcdcf20faddf95b2bd43b8e55e36b
SQARafael <rafael.chica@sqasa.co>
2023-08-22 11:15:08 -0500

    add default UI

---
+++ src/main/java/com/defaultpackage/ui/DefaultUI.java
@@ -0,0 +1,12 @@
+package com.defaultpackage.ui;
+/*
+ * @(#) DefaultUI.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class DefaultUI {
+}
